2ebf2bd6556960e636f4b973108952cfbbd3e625
psychonoob <psychonoob@gmx.ch>
2016-11-28 19:13:05 +0100

    Grobes Design erstellt TVShowTableCell mit IBOutlets

---
+++ TV_Discovery/TV_Discovery/TVShowTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TVShowTableViewCell.swift
+//  TV_Discovery
+//
+//  Created by iOSKurs on 28.11.16.
+//  Copyright © 2016 iOSKurs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TVShowTableViewCell : UITableViewCell{
+    @IBOutlet weak var imgPoster: UIImageView!
+    @IBOutlet weak var lblEpisode: UILabel!
+    @IBOutlet weak var lblRating: UILabel!
+    @IBOutlet weak var lblYear: UILabel!
+    @IBOutlet weak var lblDescr: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+
+    
+    
+    
+    
+}
