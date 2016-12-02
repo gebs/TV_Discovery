93a4b23c0cb3065081293cd7a43485fee92c07f4
psychonoob <psychonoob@gmx.ch>
2016-12-02 16:31:28 +0100

    Löschen für neu aufsetzen

--- TV_Discovery/Pods/Target Support Files/Pods-TV_Discovery/Pods-TV_Discovery-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_TV_DiscoveryVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TV_DiscoveryVersionString[];
-
